45f7d025f663d8ef3edf4fbbb4e173070a39514f
NoahFijan <noahfijan1@gmail.com>
2020-03-05 16:40:27 -0500

    encryption template

--- ciphertextMSG.py
+++ ciphertextMSG.py
@@ -1,8 +1,17 @@
 import message
 
 class CiphertextMSG(message):
-    def __init__(self, text):
-        super().message = text
+    def __init__(self, text, encryptionType):
+        if encryptionType == 'RSA':
+            super().message = rsaEncrypt(text)
+        if encryptionType == 'cesar':
+            super().message = cesarEncrypt(text)
 
+    def rsaEncrypt(text):
+        #do RSA encryption
+        pass
 
+    def cesarEncrypt(text):
+        #do cesar cypher encryption
+        pass
 
