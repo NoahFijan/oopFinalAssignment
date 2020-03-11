9475cbd71b34766b634715c68d0fe9a9988b6fab
NoahFijan <noahfijan1@gmail.com>
2020-03-11 00:11:41 -0400

    RSA keygen implemented, works for encrypting and decrypting ints, TODO: work for ascii chars

--- ciphertextMSG.py
+++ ciphertextMSG.py
@@ -1,17 +1,90 @@
-import message
+from message import Message
+import random
+import math
+import sympy
+import os
 
-class CiphertextMSG(message):
+class CiphertextMSG(Message):
     def __init__(self, text, encryptionType):
         if encryptionType == 'RSA':
-            super().__init__(rsaEncrypt(text))
+            if not os.path.exists('./pub.txt') or not os.path.exists('./priv.txt'):
+                self.generateKeys()
+
+            ciphertext = self.rsaEncrypt(text)
+            super().__init__(ciphertext)
+
         if encryptionType == 'cesar':
-            super().__init__(cesarEncrypt(text))
+            super().__init__(self.cesarEncrypt(text))
 
-    def rsaEncrypt(text):
+    def lcm(self, a, b):
+        return abs(a * b) // math.gcd(a, b)
+
+    def modInverse(self, a, m): 
+        a = a % m;  
+        for x in range(1, m): 
+            if ((a * x) % m == 1): 
+                return x 
+        return 1
+
+    def writeKeys(self, n, e, d):
+        f = open('pub.txt','w')
+        f.writelines([str(n)+'\n', str(e)])
+        f.close()
+        f = open('priv.txt','w')
+        f.write(str(d))
+        f.close()
+
+    def readPubKey(self):
+        f = open('pub.txt', 'r')
+        n = f.readline()
+        e = f.readline()
+        f.close()
+        f = open('priv.txt', 'r')
+        d = f.readline()
+        f.close()
+        return int(n), int(e), int(d)
+
+    def generateKeys(self):
+        print('generating RSA keys')
+        minP = 1000
+        maxP = 10000
+        setOfPrimes = [i for i in range(minP, maxP) if sympy.isprime(i)]
+        p = random.choice(setOfPrimes)
+        q = random.choice(setOfPrimes)
+        while q == p:
+            q = random.choice(setOfPrimes) 
+        lambdaN = self.lcm(p-1,q-1)
+        n = p*q 
+        e = 3 
+        while math.gcd(e,lambdaN)!=1:
+            e +=1 
+            if e>lambdaN:
+                e = 3 
+        d = self.modInverse(e,lambdaN)
+        self.writeKeys(n, e, d)
+        
+
+    
+    def rsaEncrypt(self, text):
         #do RSA encryption
-        pass
+        #step 2 work on ascii conversion so that messages can be encrypted, not just numbers
+        #step 3 work on decryption
+        n, e, d = self.readPubKey()
 
-    def cesarEncrypt(text):
+        m = 10000
+        print(f'message = {m}')
+        c = (m**e)%n
+        print(f'encrypted message = {c}')
+
+        dec = (c**d)%n
+
+        print(f'decrypted message: {dec}')
+
+    def cesarEncrypt(self, text):
         #do cesar cypher encryption
         pass
+
+if __name__ == '__main__':
+    ct = CiphertextMSG('m', 'RSA')
+    
 
--- message.py
+++ message.py
@@ -1,16 +1,7 @@
 class Message:
     def __init__(self, text):
-        '''
-        this innitalizes the message object
-        '''
         self.message = text
 
-    @proporty
-    def message(self):
-        return self._text
-
-    @message.setter
-    def value(self, text):
-        self._text = text
-
+    def somefunction(self):
+        pass
     
