e580156071e34accf86e0637db91260ed864e42e
NoahFijan <noahfijan1@gmail.com>
2020-03-24 15:18:55 -0400

    completed main function

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import Message
-# test
-print("pee pee poo poo")
+from message import Message
+from ciphertextMSG import CiphertextMSG
+from plaintextMSG import PlaintextMSG
+import random
+
+if __name__ == '__main__':
+    encryptionTypes = ['RSA', 'Playfair', 'Transposition', 'Product', 'Cesar', 'Substitution']
+    texts = []
+    
+    while True:
+        print('if you would like to stop running the program, type "stop"')
+        text = input('enter a message you would like to encrypt: ')
+        if text == 'stop':
+            break
+        encType = random.choice(encryptionTypes)
+        encMessage = PlaintextMsg(text, encType)
+        decMessage = ciphertextMSG(encMessage.message, encType)
+        texts.append((encMessage, decMessage, encType))
+    
+    while len(texts) != 0:
+        m = texts.pop(0)
+        print(f'message {m[1].message} was encrypted using {m[2]} and the encrypted message was {m[0].message}')
+
+
+
