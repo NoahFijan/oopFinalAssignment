79a9aae36b5407f175747d07834b4e429eab6a01
NoahFijan <noahfijan1@gmail.com>
2020-03-26 15:49:34 -0400

    finalizing main function and added comments

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+#Noah Fijan, 100749828
+#Noah Morin, 100740090
+#Blake Whiting, 100743587 
+
 from message import Message
 from ciphertextMSG import CiphertextMSG
 from plaintextMSG import PlaintextMSG
 import random
+import string
 
 if __name__ == '__main__':
     encryptionTypes = ['RSA', 'Playfair', 'Transposition', 'Product', 'Caesar', 'Substitution']
@@ -11,21 +16,25 @@ if __name__ == '__main__':
     print(" | By Noah. M, Noah. F, and Blake")
     while True:
         print('\nif you would like to stop running the program, type "stop"')
+        print('the only valid characters are the lower case ascii a-z')
         text = input('enter a message you would like to encrypt >>> ')
         if text == 'stop':
             break
-        encType = random.choice(encryptionTypes)
-        encMessage = PlaintextMSG(text, encType)
-        decMessage = CiphertextMSG(encMessage.message, encType)
-        texts.append((encMessage, decMessage, encType))
-    
-    while len(texts) != 0:
+        try:
+            for c in text:
+                if not c in string.ascii_lowercase: 
+                    raise TypeError # certain ciphers (playfair, caesar) only account for lowercase ascii, therefore we have to make sure all inputs match this criteria as it could raise an error 
+        except TypeError:
+            print('Error: please ensure you only enter lowercase letters')
+            continue
+        encType = random.choice(encryptionTypes) # randomly pick an encryption type
+        encMessage = PlaintextMSG(text, encType) # encrypt the message
+        decMessage = CiphertextMSG(encMessage.message, encType) # decrypt the message
+        texts.append((encMessage, decMessage, encType)) # append enc message, dec message, and enc type to array
+
+    while len(texts) != 0: # loop through array and print out all the decrypted and encypted strings along with the encryption method
         m = texts.pop(0)
-<<<<<<< HEAD
         print(f'message "{m[1].message}" was encrypted using {m[2]} and the encrypted message was "{m[0].message}"')
-=======
-        print(f'Message "{m[1].message}" was encrypted using the {m[2]} Cipher and the encrypted message was "{m[0].message}"')
->>>>>>> 4b27444dbfe1584138f575f534848060d45ec22e
 
 
 
